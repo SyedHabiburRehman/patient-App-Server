1588902bd2583717f6a886ba6151c11e4e8eada7
Syed Habib <syed_habib@live.com>
2017-09-18 15:44:41 +0500

    / route remove

--- routes/routes.js
+++ routes/routes.js
@@ -3,9 +3,9 @@ const AuthController = require('../controllers/authController');
 module.exports = (app) => {
     // watch for incoming requests of method GET
     // to the route http://localhost:3050/api 
-    app.get('/', (req, res, next) => {
-        res.send('hello world')
-    })
+    // app.get('/', (req, res, next) => {
+    //     res.send('hello world')
+    // })
     app.post('/api/signup', AuthController.create);
     app.get('/api/login', AuthController.get);
     // app.put('/api/drivers/:id', AuthController.edit);
